366a0d7e536742aa3ea343446cf7331ddd9d49fe
manager <mac@macdemacbook-pro.local>
2016-06-30 16:40:09 +0800

    添加到gitHub

---
+++ XMGDemo2/XMGDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMGDemo2
+//
+//  Created by Mac on 16/6/30.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMGDemo2/XMGDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMGDemo2
+//
+//  Created by Mac on 16/6/30.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
